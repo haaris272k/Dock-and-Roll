e66143dafaeb01baa2d71a7354905c6b169ba620
Haaris <89451392+haaris272k@users.noreply.github.com>
2023-09-04 15:37:38 +0530

    Update and rename hello.py to hello.go

---
+++ hello.go
@@ -0,0 +1,14 @@
+/*
+Dockerization Example: Hello World
+
+This Go program prints "Hello, World." It's used as part of a Dockerization tutorial.
+You can build a Docker image and run a container using this program by following the guide here - http://lnkiy.in/building_docker_image
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
--- hello.py
+++
@@ -1,8 +0,0 @@
-"""
-Dockerization Example: Hello World
-
-This Python script prints "Hello World." It's used as part of a Dockerization tutorial.
-You can build a Docker image and run a container using this script by following the guide here - http://lnkiy.in/building_docker_image
-"""
-
-print("Hello World")
